293eec045af39d3dc6b518a8010961ac2bb66872
Eugenia <musat.eugenia@gmail.com>
2024-04-14 13:17:45 +0200

    Add project files.

---
+++ Lexicon_MVC_Storage_11/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lexicon_MVC_Storage_11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
