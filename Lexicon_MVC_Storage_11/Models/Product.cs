749568f6f4316941f257e1d8752ee058dfab6af9
Eugenia <musat.eugenia@gmail.com>
2024-04-14 15:43:52 +0200

    Add Produc page instead of Home
    add annotation and restrictions in model

--- Lexicon_MVC_Storage_11/Models/Product.cs
+++ Lexicon_MVC_Storage_11/Models/Product.cs
@@ -1,13 +1,17 @@
-﻿namespace Lexicon_MVC_Storage_11.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon_MVC_Storage_11.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public int Price { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Orderdate { get; set; }
         public string? Category { get; set; }
         public string? Shelf { get; set; }
+        [Range(1,20)]
         public int Count { get; set; }
         public string? Description { get; set; }
     }
